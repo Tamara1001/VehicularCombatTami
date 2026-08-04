5e638a2c28a866fb2382c96936a3c267fb4d4d31
Alejo Warner <alewar2783@gmail.com>
2026-08-04 16:24:01 -0300

    Wwise

---
+++ VehicularCombatTami_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DISPARO_E = 318604567U;
+        static const AkUniqueID DISPARO_J = 318604568U;
+        static const AkUniqueID PAUSA = 3092587489U;
+        static const AkUniqueID PLAY_HIDOINA = 3071699590U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID DISPARO_E = 318604567U;
+        static const AkUniqueID DISPARO_J = 318604568U;
+        static const AkUniqueID HYPEREPICSUPERSPACIALMUSIC = 1569824145U;
+        static const AkUniqueID PAUSA = 3092587489U;
+        static const AkUniqueID VEHICULARCOMBAT = 1240941068U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
